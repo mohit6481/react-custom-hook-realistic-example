c207ffd9ca97ffcd7724c10948e6f50beed5b9f1
mohit6481 <mohit.rathi6481@gmail.com>
2023-01-25 20:30:13 +0530

    react custom hook for making http request

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import useHttp from "./hooks/use-http";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const transformTask = (tasks) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasks) {
+      loadedTasks.push({ id: taskKey, text: tasks[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  };
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  // const fetchTasks = async (taskText) => {
+  //   try {
+  //     const response = await fetch(
+  //       "https://react-http-3065c-default-rtdb.firebaseio.com/tasks.json"
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error("Request failed!");
+  //     }
+
+  //     const data = await response.json();
+
+  //     const loadedTasks = [];
+
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || "Something went wrong!");
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  useEffect(() => {
+    fetchTasks({
+      url: "https://react-http-3065c-default-rtdb.firebaseio.com/tasks.json",
+    }, transformTask);
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
   );
 }
 
